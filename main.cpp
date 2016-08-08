5c7aac3dedde1241cf2896f7b2ec75beb5ccf387
Adi Shavit <adishavit@gmail.com>
2016-08-08 21:33:24 +0300

    Updates example.

--- main.cpp
+++ main.cpp
@@ -1,64 +1,28 @@
 #include <iostream>
 
-#include "argh.h"
-
-/*
-int main(int argc, char* argv[])
-{
-    argh::parser cmdline;
-    cmdline.parse(argc, argv);
-
-    if (cmdline["-v"])
-        std::cout << "Verbose, I am." << std::endl;
-
-    return EXIT_SUCCESS;
-}
-*/
-
-
 using namespace std;
+
+#include "argh.h"
 
 int main(int argc, const char* argv[])
 {
     argh::parser cmdl;
-    cmdl.add_param("-f");
-    cmdl.parse(argc, argv);
+    cmdl.parse(argc, argv, argh::parser::PREFER_PARAM_FOR_UNREG_OPTION);
 
-    if (cmdl["-f"])
-        cout << "Got -f" << endl;
+    if (cmdl["-v"])
+        cout << "Verbose, I am." << endl;
 
-    cout << cmdl["f"] << endl;
-
-    int fval = -1;
-    if (cmdl("f") >> fval)
-        cout << fval << endl;
-
-    cmdl("xxxx") >> (fval = -999);
-    cout << fval << endl;
-
-    cout << "Flags:\n";
-    for (auto& flag : cmdl.flags())
-        cout << "\t" << flag << endl;
-
-    cout << "Options:\n";
-    for (auto& param : cmdl.params())
-        cout << "\t" << param.first << " = " << param.second << endl;
-
-    int k = -1;
-    if (cmdl(1) >> k)
-        cout << "First pos arg is " << k << endl;
-    else
-        cout << "Could not convert first arg :-(  : '" << cmdl[1] << "', k == " << k << endl;
-
-    cout << "Free args:\n";
+    cout << "Positional args:\n";
     for (auto& pos_arg : cmdl.pos_args())
-        cout << "\t" << pos_arg << endl;
+        cout << '\t' << pos_arg << endl;
 
-    cout << "Free args 2:\n";
-    for (int i=0; i<cmdl.pos_args().size(); ++i)
-        cout << "\t" << cmdl(i).str() << endl;
+    cout << "\nFlags:\n";
+    for (auto& flag : cmdl.flags())
+        cout << '\t' << flag << endl;
+
+    cout << "\nParameters:\n";
+    for (auto& param : cmdl.params())
+        cout << '\t' << param.first << " : " << param.second << endl;
 
     return EXIT_SUCCESS;
 }
-
-
